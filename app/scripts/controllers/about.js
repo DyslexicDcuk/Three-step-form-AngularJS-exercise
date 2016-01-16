a414fea345e1f3983607f15834e6b51360ec6e17
Damir <dsehic@gmail.com>
2016-01-16 13:49:46 +0100

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exerciseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the exerciseApp
+ */
+angular.module('exerciseApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exerciseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the exerciseApp
+ */
+angular.module('exerciseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
