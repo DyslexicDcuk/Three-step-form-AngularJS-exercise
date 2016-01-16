f0a3b941be57290c03e1d46156fc83b639083839
DyslexicDcuk <dsehic@gmail.com>
2016-01-16 22:27:08 +0100

    3 step validation fix

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -10,23 +10,22 @@ angular.module('exerciseApp')
   .controller('RegistrationCtrl', ['$scope', '$state', function($scope, $state) {
     
     $scope.formData = {};
-
+    $scope.personalSubmitted = true;
     $scope.submitForm = function() {
       // check to make sure the form is completely valid
       if ($scope.userForm.$valid) {
         window.alert('our form is amazing');
       }
     };
-
+    
     $scope.nextStep = function(isValid, location) {
-      if(isValid) {
-        if (location === 'personal') {
-          $state.go('registration.car');
-        } else if (location === 'car') {
-          $state.go('registration.payment');
-        } else if (location === 'payment') {
-          $state.go('registration.thanks');
-        }
+      console.log(isValid, location);
+      if (location === 'personal') {
+        if (isValid) $state.go('registration.car');
+      } else if (location === 'car') {
+        if (isValid) $state.go('registration.payment');
+      } else if (location === 'payment') {
+        if (isValid) $state.go('registration.thanks');
       }
     };
 
