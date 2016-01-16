c914908bf11de436b8c55b72ade5dcca7b895d14
DyslexicDcuk <dsehic@gmail.com>
2016-01-16 19:00:11 +0100

    validation v1

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -7,12 +7,27 @@
  * Controller of the exerciseApp
  */
 angular.module('exerciseApp')
-  .controller('RegistrationCtrl', ['$scope', function($scope) {
-
+  .controller('RegistrationCtrl', ['$scope', '$state', function($scope, $state) {
+    
     $scope.formData = {};
 
-    $scope.processForm = function() {
-      window.alert('wee!');
+    $scope.submitForm = function() {
+      // check to make sure the form is completely valid
+      if ($scope.userForm.$valid) {
+        window.alert('our form is amazing');
+      }
+    };
+
+    $scope.nextStep = function(isValid, location) {
+      if(isValid) {
+        if (location === 'personal') {
+          $state.go('registration.car');
+        } else if (location === 'car') {
+          $state.go('registration.payment');
+        } else if (location === 'payment') {
+          $state.go('registration.thanks');
+        }
+      }
     };
 
   }]);
