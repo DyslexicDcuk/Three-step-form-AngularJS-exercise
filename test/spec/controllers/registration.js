a6daf80f74224c30ec7cd0cd2c128f84fb03beea
DyslexicDcuk <dsehic@gmail.com>
2016-01-18 00:24:16 +0100

    kinda responsive

--- test/spec/controllers/registration.js
+++ test/spec/controllers/registration.js
@@ -18,6 +18,6 @@ describe('Controller: RegistrationCtrl', function () {
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(RegistrationCtrl.awesomeThings.length).toBe(3);
+    expect(scope.carMakeOptions.length).toBe(3);
   });
 });
